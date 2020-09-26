88d035247437d44c746a285d8b42403bc5d9db5c
kelvin <klhartono@ust.hk>
2020-09-26 17:26:23 +0800

    added fruit basket

---
+++ codeitsuisse/routes/fruitbasket.py
@@ -0,0 +1,23 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+import random
+logger = logging.getLogger(__name__)
+
+def fruity(data):
+  i = 0
+  res = 0
+  for el in data:
+    x = random.randint(0,1000)
+    logging.info(f"{i} : {x}")
+    res+=x*data[el]
+  return res
+
+@app.route('/social_distancing', methods=['POST'])
+def social():
+  data = request.get_json()["tests"]
+  logging.info("data sent for evaluation {}".format(data))
+  return json.dump(fruity(data))
