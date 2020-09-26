a0ba76c40c57393f2efbc0e7a0e30bb0f0a684a4
kelvin <klhartono@ust.hk>
2020-09-27 03:10:24 +0800

    modified smsl

--- codeitsuisse/routes/snake.py
+++ codeitsuisse/routes/snake.py
@@ -11,7 +11,6 @@ def findMoves(data):
   bsize,numPlayers = data["boardSize"],data["players"]
   jumpsGood = {}
   jumpsBad = {}
-  goodMove = False
   for jump in data["jumps"]:
     temp = jump.split(":")
     temp[0],temp[1]=int(temp[0]),int(temp[1])
@@ -22,8 +21,10 @@ def findMoves(data):
         jumpsGood[temp[1]]=temp[0]
       else:
         jumpsGood[temp[0]]=temp[1]
-  currMainCell,currOtherCell, moves = 1,1, []
+  currMainCell= 1
+  moves =[]
   while currMainCell<bsize:
+    print(currMainCell,"-",end="")
     if bsize-currMainCell<=6:
       for _ in range(numPlayers-1):
         for i in range(1,7):
@@ -44,7 +45,6 @@ def findMoves(data):
           bestMove = (i,currMainCell+i+6,False)
           stay = True
       elif currMainCell+i in jumpsBad:
-        print(currMainCell+i," is in bad")
         continue
       else:
         if i+currMainCell>bestMove[1]:
@@ -54,39 +54,7 @@ def findMoves(data):
     currMainCell = currMainCell+bestMove[0]
     if bestMove[2]:
       currMainCell = bestMove[1]
-    if not stay:
-      goodMove = not goodMove
-    print("good",bestMove[1],currMainCell,bestMove[0])
-    # else:
-    #   print("bad")
-    #   worstMove=(1,currOtherCell+1,False)
-    #   stay = False
-    #   for i in range(1,7):
-    #     if currOtherCell+i in jumpsBad:
-    #       if jumpsBad[currOtherCell+i]!=0 and jumpsBad[currOtherCell+i]<worstMove[1]:
-    #         worstMove=(i,jumpsBad[currOtherCell+i],True)
-    #         stay = False
-    #       elif jumpsBad[currOtherCell+i]==0:
-    #         worstMove = (i,currOtherCell,False)
-    #         stay = True
-    #     elif currOtherCell+i in jumpsGood:
-    #       continue
-    #     else:
-    #       if i+currOtherCell<worstMove[1]:
-    #         worstMove=(i,i+currOtherCell,False)
-    #   for _ in range(numPlayers-1):
-    #     moves.append(worstMove[0])
-    #   currOtherCell = currOtherCell+worstMove[0]
-    #   if worstMove[2]:
-    #     currOtherCell = worstMove[1]
-    #   if stay:
-    #     for _ in range(numPlayers-1):
-    #       moves.append(worstMove[0])
-    #     currOtherCell-=worstMove[0]
-    #   goodMove = not goodMove
-  # print(jumpsBad)
-  # print(jumpsGood)
-  # print(currMainCell,currOtherCell,len(moves))
+  print(currMainCell,len(moves))
   return moves
 
             
