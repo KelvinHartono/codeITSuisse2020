955695e4751bfdcb673cd5af7daf8b64d1d5b3ac
kelvin <klhartono@ust.hk>
2020-09-26 21:18:32 +0800

    modified clean Floor

--- codeitsuisse/routes/cleanFloor.py
+++ codeitsuisse/routes/cleanFloor.py
@@ -8,32 +8,33 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 def cleanFloor(data):
-  # leftMost = -1
-  # for index, val in enumerate(data):
-  #   if val == 1:
-  #     leftMost = index
-  # if leftMost == -1:
-  #   return 0
   curr = 1
   data[1]= data[1]-1 if data[1]>0 else data[1]+1
   step = 1
   while curr<len(data):
-    print(data,step,curr)
     if data[curr-1]>0:
-      data[curr-1]-=1
-      if data[curr]==0 and curr==len(data)-1:
-        step+=1
-        break
-      data[curr] = data[curr]-1 if data[curr]>0 else data[curr]+1
-      step+=2
-    else:
-      curr+=1
-      if curr>=len(data):
-        break
-      data[curr]= data[curr]-1 if data[curr]>0 else data[curr]+1
-      step+=1
+      if curr == len(data)-1 and data[curr]==0:
+        step-=1
+      step += data[curr-1]*2
+      data[curr]=data[curr]-data[curr-1]
+      data[curr-1]=0
+      if data[curr]<0:
+        if data[curr]==-1 and curr==len(data)-1:
+          step-=1
+          break
+        if data[curr]%2==0:
+          data[curr]=0
+        else:
+          data[curr]=1 
+    curr+=1
+    if curr>len(data)-1 or (data[curr]==0 and curr==len(data)-1):
+      break
+    data[curr]= data[curr]-1 if data[curr]>0 else data[curr]+1
+    step+=1
+
   if data[-1]>0:
     step+=data[-1]*2+1
+  print(step)
   return step
 
 @app.route('/clean_floor', methods=['POST'])
